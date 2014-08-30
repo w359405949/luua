3bc938127206308a3bfe56a7f40da829439f9d53
w <w359405949@gmail.com>
2014-08-30 21:06:41 +0800

    back

---
+++ src/luv_prepare.c
@@ -0,0 +1,59 @@
+#include <lauxlib.h>
+#include <uv.h>
+#include "luv_misc.h"
+#include "luv_prepare.h"
+#include "utils.h"
+
+int luv_prepare_destroy(lua_State* L)
+{
+    uv_prepare_t* handle = (uv_prepare_t*)luaL_checkudata(L, 1, UV_PREPARE_METATABLE_NAME);
+    uv_prepare_stop(handle);
+    return 0;
+}
+
+int luv_prepare_new(lua_State* L)
+{
+    uv_loop_t* loop = (uv_loop_t*)luaL_checkudata(L, 1, UV_LOOP_METATABLE_NAME);
+    uv_prepare_t* handle = (uv_prepare_t*)lua_newuserdata(L, sizeof(uv_prepare_t));
+    uv_prepare_init(loop, handle);
+    lua_pushlightuserdata(L, handle);
+
+    /* metatable __gc */
+    luua_setgcmetamethod(L, UV_PREPARE_METATABLE_NAME, luv_prepare_destroy);
+}
+
+void prepare_cb(uv_prepare_t* handle)
+{
+    lua_State* coroutine = (lua_State*)handle->data;
+    lua_State* parent = (lua_State*)lua_touserdata(coroutine, -1);
+    lua_pop(coroutine, 1);
+    int result = lua_resume(coroutine, parent, 1);
+    if (result > 1) {
+        luua_stackDump(coroutine);
+        uv_stop(handle->loop);
+    }
+}
+
+int luv_prepare_start(lua_State* L)
+{
+    uv_prepare_t* handle = (uv_prepare_t*)luaL_checkudata(L, 1, UV_PREPARE_METATABLE_NAME);
+    lua_getfield(L, 2, "coroutine");
+    lua_getfield(L, 2, "parent");
+    lua_State* coroutine = lua_tothread(L, 3);
+    lua_State* parent = lua_tothread(L, 4);
+
+    lua_settop(L, 2);
+    lua_xmove(L, coroutine, 1);
+
+    lua_pushlightuserdata(coroutine, parent);
+    handle->data = coroutine;
+    uv_prepare_start(handle, prepare_cb);
+    return 0;
+}
+
+int luv_prepare_stop(lua_State* L)
+{
+    uv_prepare_t* handle = (uv_prepare_t*)luaL_checkudata(L, 1, UV_PREPARE_METATABLE_NAME);
+    uv_prepare_stop(handle);
+    return 0;
+}
---
+++ src/luv_prepare.h
@@ -0,0 +1,11 @@
+#ifndef LUV_PREPARE
+#define LUV_PREPARE
+#include <lua.h>
+
+#define UV_PREPARE_METATABLE_NAME "uv.uv_prepare_t"
+
+int luv_prepare_new(lua_State* L);
+int luv_prepare_start(lua_State* L);
+int luv_prepare_stop(lua_State* L);
+
+#endif /* LUV_PREPARE */
