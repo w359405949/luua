76213fc21c7271734bb79bc4f1bc569cdf969f2a
w359405949 <w359405949@gmail.com>
2014-09-01 09:53:39 +0800

    fix luaL_check uv.uv_loop_t problem

--- src/luv_misc.c
+++ src/luv_misc.c
@@ -7,14 +7,13 @@ int luv_loop_destroy(lua_State* L)
 {
     uv_loop_t* loop = (uv_loop_t*)luaL_checkudata(L, 1, UV_LOOP_METATABLE_NAME);
     uv_stop(loop);
-    uv_loop_delete(loop);
     return 0;
 }
 
 int luv_loop_new(lua_State* L)
 {
-    uv_loop_t *loop = uv_loop_new();
-    lua_pushlightuserdata(L, loop);
+    uv_loop_t *loop = (uv_loop_t*)lua_newuserdata(L, sizeof(uv_loop_t));
+    uv_loop_init(loop);
 
     /* metatable __gc */
     luua_setgcmetamethod(L, UV_LOOP_METATABLE_NAME, luv_loop_destroy);
@@ -23,17 +22,12 @@ int luv_loop_new(lua_State* L)
 
 int luv_run(lua_State* L)
 {
+    lua_settop(L, 3);
     uv_loop_t* loop = (uv_loop_t*)luaL_checkudata(L, 1, UV_LOOP_METATABLE_NAME);
-    int flags = 0;
-    int nowait = 0;
-    int once = 0;
+    int flags = UV_RUN_DEFAULT;
+    flags |= lua_toboolean(L, 2);
+    flags |= lua_toboolean(L, 3);
 
-    if (nowait) {
-        flags |= nowait;
-    }
-    if (once) {
-        flags |= once;
-    }
     uv_run(loop, flags);
     return 0;
 }
